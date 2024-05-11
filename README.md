40d515c948d9470e2fa20387ce9d960296b87327
ShayCohenn <shay91847@gmail.com>
2024-05-11 17:56:09 +0300

    commit 1

---
+++ README.md
@@ -0,0 +1,5 @@
+# Blogs Website
+
+### A basic blog website with user authentication and roles, full CRUD operations - with GET, POST - uploading posts and comments, PUT - editing existing ones and DELETE - deleting old ones
+### This website uses Flask for its backend and basic HTML, CSS and JavaScript with Jinja2 for its frontend
+### For the database I used PostgreSQL deployed on AWS RDS 
---
+++ models.py
@@ -0,0 +1,38 @@
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Integer, String, Text
+from sqlalchemy.orm import relationship, DeclarativeBase, Mapped, mapped_column
+from flask_login import UserMixin
+
+class Base(DeclarativeBase):
+    __abstract__ = True
+db: SQLAlchemy = SQLAlchemy(model_class=Base)
+
+class BlogPost(db.Model):
+    __tablename__ = "blog_posts"
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    author_id: Mapped[int] = mapped_column(Integer, db.ForeignKey("users.id"))
+    author = relationship("User", back_populates="posts")
+    title: Mapped[str] = mapped_column(String(250), unique=True, nullable=False)
+    subtitle: Mapped[str] = mapped_column(String(250), nullable=False)
+    date: Mapped[str] = mapped_column(String(250), nullable=False)
+    body: Mapped[str] = mapped_column(Text, nullable=False)
+    img_url: Mapped[str] = mapped_column(String(250), nullable=False)
+    comments = relationship("Comment", back_populates="parent_post", cascade="all, delete-orphan")
+
+class User(UserMixin, db.Model):
+    __tablename__ = "users"
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    email: Mapped[str] = mapped_column(String(100), unique=True)
+    password: Mapped[str] = mapped_column(String(100))
+    name: Mapped[str] = mapped_column(String(100))
+    posts = relationship("BlogPost", back_populates="author")
+    comments = relationship("Comment", back_populates="comment_author")
+
+class Comment(db.Model):
+    __tablename__ = "comments"
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    text: Mapped[str] = mapped_column(Text, nullable=False)
+    author_id: Mapped[int] = mapped_column(Integer, db.ForeignKey("users.id"))
+    comment_author = relationship("User", back_populates="comments")
+    post_id: Mapped[str] = mapped_column(Integer, db.ForeignKey("blog_posts.id"))
+    parent_post = relationship("BlogPost", back_populates="comments")
